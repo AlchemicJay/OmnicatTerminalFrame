a4cb769f39edf735f359a27a183e96eb6dae934d
Nuanced1 <79663378+Nuanced1@users.noreply.github.com>
2024-02-06 00:11:33 -0500

    Update route.ts

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -11,7 +11,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
       return new NextResponse(`<!DOCTYPE html><html><head>
     <title>This is frame 3</title>
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/Qme4FXhoxHHfyzTfRxSpASbMF8kajLEPkRQWhwWu9pkUjm/3.png" />
+    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmV6JaDnQ8CPZCsRjXBEvdNcx4jd4M2389EZz19pUatUrp/3.png" />
     <meta property="fc:frame:button:1" content="Visit omnicat.xyz" />
     <meta property="fc:frame:button:1:action" content="post_redirect" />
     <meta property="fc:frame:button:2" content="Buy Omnicat on Base" />
@@ -24,7 +24,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   return new NextResponse(`<!DOCTYPE html><html><head>
     <title>This is frame ${id}</title>
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/Qme4FXhoxHHfyzTfRxSpASbMF8kajLEPkRQWhwWu9pkUjm/${id}.png" />
+    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmV6JaDnQ8CPZCsRjXBEvdNcx4jd4M2389EZz19pUatUrp/${id}.png" />
     <meta property="fc:frame:button:1" content="He Is Everywhere" />
     <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=${nextId}" />
   </head></html>`);
